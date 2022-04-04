b9fd7d72e83b09cbb9a3abc6d7a4233582a90753
Sebastián Martínez <sebamar88@gmail.com>
2022-04-04 01:52:40 -0300

    port changed

--- Desafio-3/app.js
+++ Desafio-3/app.js
@@ -1,23 +1,29 @@
 const express = require("express");
+
+// utils
 const getRandomItem = require("./utils/functions");
 const path = "./text.json";
 
 // Import Class and Instantiate
 const Container = require("./utils/classes/Container");
 const container = new Container(path);
+
 // Data from text.json
 const data = container.getAllItems();
 
+// Create Express App
 const app = express();
 
+// GET /products
 app.get("/products", async (req, res) => {
   res.json(await data);
 });
-
+// GET /randomProduct
 app.get("/randomProduct", async (req, res) => {
   res.json(getRandomItem(await data));
 });
 
-app.listen(3000, () => {
+// Listen on port 8080
+app.listen(8080, () => {
   console.log("Example app listening on port 3000!");
 });
