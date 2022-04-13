0530da1db4dd3bab334d7aac38204b359cf996f0
Sebastian <sebamar88@gmail.com>
2022-04-12 21:21:19 -0300

    added bodyparser middleware

--- Desafio-4/api/routes.js
+++ Desafio-4/api/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { Router } = express;
-const bodyParser = require("body-parser");
 
 // Products Controllers
 const getAll = require("./controllers/products/getAll");
@@ -9,16 +8,13 @@ const save = require("./controllers/products/saveProduct");
 const update = require("./controllers/products/updateProduct");
 const deleteById = require("./controllers/products/deleteById");
 
-// create application/json parser
-const jsonParser = bodyParser.json();
-
 const router = new Router();
 
 // Products Routes
 router.get("/products", getAll);
 router.get("/products/:id", getById);
-router.post("/products", jsonParser, save);
-router.put("/products/:id", jsonParser, update);
-router.delete("/products/:id", jsonParser, deleteById);
+router.post("/products", save);
+router.put("/products/:id", update);
+router.delete("/products/:id", deleteById);
 
 module.exports = router;
--- Desafio-4/app.js
+++ Desafio-4/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const routes = require("./api/routes");
+const bodyParser = require("body-parser");
 
 // Create Express App
 const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api", routes);
 
