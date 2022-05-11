edd7dbf2448502f49feed7e489e2d8a3a01d017c
Sebastian <sebamar88@gmail.com>
2022-05-11 16:18:26 -0300

    changed deleteProductFromCart

--- 1era-preentrega/api/controllers/cart/deleteProductFromCart.js
+++ 1era-preentrega/api/controllers/cart/deleteProductFromCart.js
@@ -5,10 +5,7 @@ const cart = new Cart(path);
 const deleteProductFromCart = async (req, res) => {
   const { cartId, productId } = req.params;
   try {
-    const cartFinded = await cart.deleteProductFromCart(
-      Number(cartId),
-      Number(productId)
-    );
+    await cart.deleteProductFromCart(Number(cartId), Number(productId));
     res.status(200).json({ message: "Product deleted from cart" });
   } catch (error) {
     res.status(500).json({ message: error.message });
